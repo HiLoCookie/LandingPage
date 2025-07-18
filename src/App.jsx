a6fa0cc0e5feeea177649baa3eeaffcdb8fcc24e
HiLoCookie <vyskocilova.eliska3@gmail.com>
2025-07-18 17:13:18 +0200

    Prepare for GitHub Pages Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
+import myPic from './assets/gradient.png';
 
 export default function App() {
 
@@ -16,7 +17,7 @@ export default function App() {
   return (
     <main>
       {/* Gradient Image */}
-      <img className="absolute top-0 right-0 opacity-80 z-10" src="src\assets\gradient.png" alt="Gradeint-img" />
+      <img className="absolute top-0 right-0 opacity-80 z-10" src={myPic} alt="Gradeint-img" />
 
       {/* Blur Effect */}
       <div className="absolute top-[20%] right-[-5%] w-[40rem] h-10 bg-[#e99b63] opacity-50 blur-3xl rotate-[-30deg] -z-10"></div>
